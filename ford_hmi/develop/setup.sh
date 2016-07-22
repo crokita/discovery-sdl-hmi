8c4f6e8272b1bff054105cde565fcc6051aaf3a5
Christopher Rokita <chris@livio.io>
2016-07-22 14:30:36 -0400

    Make develop HMI branch

--- ford_hmi/develop/setup.sh
+++ ford_hmi/develop/setup.sh
@@ -10,6 +10,6 @@ echo "Changing Flags.js HMI ServerAddress to ${HMI_WEBSOCKET_IP}"
 
 # Replace IP and Port in flags file to match the machine's IP address
 perl -pi -e 's/127.0.0.1:8087/'$HMI_WEBSOCKET_IP':8087/g' /var/www/app/Flags.js
-
+cat /var/www/app/Flags.js
 # Start nginx
 /usr/sbin/nginx
